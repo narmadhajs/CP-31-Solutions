113913cb1e0707c82f4236c596cf12b5664abe3b
Narmadha J S <138014247+narmadhajs@users.noreply.github.com>
2024-06-17 19:34:52 +0530

    Create GameWithIntegers.cpp

---
+++ Rating-800/GameWithIntegers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+    int t;
+   cin>>t;
+   while(t--){
+       int n;
+       cin>>n;
+       if(n%3==0){
+           cout<<"Second"<<endl;
+       }
+       else{
+           cout<<"First"<<endl;
+       }
+   }
+
+}
