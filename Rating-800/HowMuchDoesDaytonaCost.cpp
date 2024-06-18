d3c3f7feb204c5189c0f0b0b9b5f0f786b1c8ae5
Narmadha J S <138014247+narmadhajs@users.noreply.github.com>
2024-06-18 12:33:05 +0530

    Update HowMuchDoesDaytonaCost.cpp

--- Rating-800/HowMuchDoesDaytonaCost.cpp
+++ Rating-800/HowMuchDoesDaytonaCost.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int m,n;
+        cin>>m>>n;
+        int x,flag=0;
 
+        for(int i=0;i<m;i++){
+            cin>>x;
+            if(n==x){
+                flag=1;
+
+            }
+        }
+       
+if(flag==0){
+    cout<<"NO"<<endl;
+}
+      
+else{
+    cout<<"YES"<<endl;
+}
+
+    }
+}
